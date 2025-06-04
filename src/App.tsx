7929a9b53c9108a0f585ddb041ac3eb265dec6e1
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-06-04 11:27:55 +0200

    chore: add API token for base map

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import AutoSizer from "react-virtualized/dist/commonjs/AutoSizer";
 import { injectComponents, SidePanelFactory } from "@kepler.gl/components";
 import { KeplerGlState } from "@kepler.gl/reducers";
 import { Action, Dispatch } from "redux";
+
 import CustomSidePanelFactory from "./components/kepler/side-panel";
 
 // Inject custom components
 const KeplerGl = injectComponents([
   [SidePanelFactory, CustomSidePanelFactory] as never,
 ]);
-
+const ApiAccessToken= import.meta.env.VITE_BASE_MAP_TOKEN
 const App = () => {
   return (
     <div className="w-full h-full rounded-lg overflow-hidden">
       <AutoSizer>
         {({ height, width }) => (
-          <KeplerGl width={width} height={height} theme="light" />
+          <KeplerGl width={width} height={height} theme="light"  mapboxApiAccessToken={ApiAccessToken} />
         )}
       </AutoSizer>
     </div>
