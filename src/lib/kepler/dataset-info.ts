8b1a9644f3a505bad2896c91b190efdd2580bfa6
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-06-12 15:55:12 +0200

    feat: add datasets-info

---
+++ src/lib/kepler/dataset-info.ts
@@ -0,0 +1,15 @@
+export const DISTANCES_FLOWMAP_INFO = {
+  title: "Overtaking Distances",
+  description: "Overtaking distances flowmap data",
+  url: "https://api.atrai.bike/collections/distances_flowmap/items?f=json&limit=1000000",
+  configUrl:
+    "https://github.com/reedu-reengineering-education/atrai-bike-kepler/tree/main/src/lib/kepler/config-distances.json",
+};
+
+export const ROAD_ROUGHNESS_INFO = {
+  title: "Road Roughness Data",
+  description: "Data showing road roughness in the city",
+  url: "https://api.atrai.bike/collections/road_roughness/items?f=json&limit=1000000",
+  configUrl:
+    "https://github.com/reedu-reengineering-education/atrai-bike-kepler/tree/main/src/lib/kepler/config.json",
+};
