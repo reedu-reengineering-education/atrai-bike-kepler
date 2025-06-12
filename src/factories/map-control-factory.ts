59a01d155275ff8f2d31e869d4e88321a3b04b72
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-06-12 16:44:57 +0200

    refactor: fix the type issues

--- src/factories/map-control-factory.ts
+++ src/factories/map-control-factory.ts
@@ -12,16 +12,14 @@ import { MapControlComponent } from "./map-control";
 function CustomMapControlFactory(
   EffectControl: ComponentType,
   EffectManager: ComponentType,
-  //   SqlPanelControl: ComponentType,
-
-  ...deps: ComponentType[]
+  ...deps: [...Parameters<typeof MapControlFactory>]
 ) {
   const MapControl = MapControlFactory(...deps);
   const actionComponents = [
     ...(MapControl.defaultActionComponents ?? []),
     EffectControl,
   ];
-
+  // @ts-expect-error: MapControlComponent requires internal typing that's not exposed
   return MapControlComponent(MapControl, EffectManager, actionComponents);
 }
 
